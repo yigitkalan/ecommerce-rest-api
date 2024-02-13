cf69bf97abe2f305bb2684f54a1e85f212a937b5
yigitkalan <sametykalan@gmail.com>
2024-02-14 01:49:42 +0300

    fix: remove unnnecessary onUpdate statements

--- Infrastructure/Ecommerce.Persistance/Migrations/20240213193443_InitialCreate.cs
+++ Infrastructure/Ecommerce.Persistance/Migrations/20240213193443_InitialCreate.cs
@@ -108,14 +108,12 @@ namespace Ecommerce.Persistance.Migrations
                         column: x => x.CategoriesId,
                         principalTable: "Categories",
                         principalColumn: "Id",
-                        onUpdate: ReferentialAction.SetNull,
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_CategoryProduct_Products_ProductsId",
                         column: x => x.ProductsId,
                         principalTable: "Products",
                         principalColumn: "Id",
-                        // onUpdate: ReferentialAction.SetNull,
                         onDelete: ReferentialAction.Cascade);
                 });
 
